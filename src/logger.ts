4ec9e4c70566b06ad1f7522586cd1235cc9d8f20
Blake Morgan <blake_morgan@byu.edu>
2021-07-21 10:35:08 -0400

    Remove isDeployed

--- src/logger.ts
+++ src/logger.ts
@@ -1,7 +1,6 @@
 import Pino, { LoggerOptions, Logger } from 'pino'
 
 export default function DefaultLogger (options?: LoggerOptions): Logger {
-  const isDeployed = process.env.NODE_ENV === 'production'
   return Pino({
     level: (() => {
       switch (process.env.NODE_ENV) {
@@ -23,7 +22,7 @@ export default function DefaultLogger (options?: LoggerOptions): Logger {
       paths: ['req.headers.authorization', 'req.headers.assertion', 'req.headers["x-jwt-assertion"]', 'req.headers["x-jwt-assertion-original"]'],
       censor: '***'
     },
-    prettyPrint: isDeployed ? false : {
+    prettyPrint: process.env.NODE_ENV === 'production' ? false : {
       // if in local environment use pretty print logs
       translateTime: 'UTC:yyyy-mm-dd\'T\'HH:MM:ss.l\'Z\'' // show timestamp instead of epoch time
     },
