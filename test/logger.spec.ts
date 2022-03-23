ad1e4aad4b4c10c8f0da26f4b7fb300db1ee587e
Gary Crye <garygsc@gmail.com>
2022-03-23 08:53:48 -0600

    Use `.skip` instead of commenting out useful flaky tests
    This should increase their visibility while still keeping them from
    failing our (now sparse) test suite.

--- test/logger.spec.ts
+++ test/logger.spec.ts
@@ -25,50 +25,50 @@ afterEach(() => {
 
 // Pino v7+ implements transports which are problematic in Jest environments.
 // Docs: https://github.com/pinojs/pino-pretty#usage-with-jest
-// describe('In local env', () => {
-//   beforeEach(() => {
-//     process.env.NODE_ENV = 'local'
-//     logger = DefaultLogger()
-//   })
-//
-//   test('default logger should default to debug level', () => {
-//     logger.debug('debug works')
-//
-//     expect(logger.level).toEqual('debug')
-//     expect(logged).toContain('DEBUG') // must contain the debug level
-//     expect(logged).toContain('debug works') // must contain the message
-//   })
-//
-//   test('default logger should still display info', () => {
-//     logger.info('info works')
-//
-//     expect(logged).toContain('INFO') // must contain the info level
-//     expect(logged).toContain('info works') // must contain the message
-//   })
-//
-//   test('default logger displays logs with iso datetime format', () => {
-//     logger.info('iso date works')
-//
-//     expect(logged).toContain(`[${jan1st.toISOString()}]`)
-//   })
-//
-//   test('default logger displays logs in pretty printed format', () => {
-//     logger.info('pretty print works')
-//
-//     expect(logged).not.toContain('{')
-//   })
-//
-//   // TODO - Figure out how to stub `require` to throw a fake MODULE_NOT_FOUND error
-//   // test('default logger does not pretty print if pino-pretty is not installed', async () => {
-//   //   proxyquire('../src/logger', {
-//   //     'pino-pretty': null
-//   //   })
-//   //
-//   //   const logger = DefaultLogger()
-//   //   logger.info('pretty print disabled')
-//   //   expect(logged).toContain('{')
-//   // })
-// })
+describe.skip('In local env', () => {
+  beforeEach(() => {
+    process.env.NODE_ENV = 'local'
+    logger = DefaultLogger()
+  })
+
+  test('default logger should default to debug level', () => {
+    logger.debug('debug works')
+
+    expect(logger.level).toEqual('debug')
+    expect(logged).toContain('DEBUG') // must contain the debug level
+    expect(logged).toContain('debug works') // must contain the message
+  })
+
+  test('default logger should still display info', () => {
+    logger.info('info works')
+
+    expect(logged).toContain('INFO') // must contain the info level
+    expect(logged).toContain('info works') // must contain the message
+  })
+
+  test('default logger displays logs with iso datetime format', () => {
+    logger.info('iso date works')
+
+    expect(logged).toContain(`[${jan1st.toISOString()}]`)
+  })
+
+  test('default logger displays logs in pretty printed format', () => {
+    logger.info('pretty print works')
+
+    expect(logged).not.toContain('{')
+  })
+
+  // TODO - Figure out how to stub `require` to throw a fake MODULE_NOT_FOUND error
+  // test('default logger does not pretty print if pino-pretty is not installed', async () => {
+  //   proxyquire('../src/logger', {
+  //     'pino-pretty': null
+  //   })
+  //
+  //   const logger = DefaultLogger()
+  //   logger.info('pretty print disabled')
+  //   expect(logged).toContain('{')
+  // })
+})
 
 describe('In production env', () => {
   beforeEach(() => {
