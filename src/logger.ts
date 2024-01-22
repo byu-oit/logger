ec02533e9319a447ce70460d607ec3a3f055d86b
jay-bulk <rbulkley@byu.edu>
2024-01-22 14:54:14 -0700

    fix: esm syntax

--- src/logger.ts
+++ src/logger.ts
@@ -1,15 +1,15 @@
 import { Logger, LoggerOptions, pino } from 'pino'
-import { getLevel, isInstalled, isProduction } from './util'
+import { getLevel, isInstalled, isProduction } from './util.js'
 import deepmerge from 'deepmerge'
 
-export function ByuLogger (options?: LoggerOptions): Logger {
+export default function ByuLogger(options?: LoggerOptions): Logger {
   const defaultOptions: LoggerOptions = {
     level: getLevel(process.env.NODE_ENV),
     messageKey: 'message',
     formatters: {
       level: level => ({ level }) // display the level not the number value of the level
     },
-    base: { }, // don't display the process pid, nor hostname
+    base: {}, // don't display the process pid, nor hostname
     redact: {
       // redact bearer tokens and JWTs
       paths: ['req.headers.authorization', 'req.headers.assertion', 'req.headers["x-jwt-assertion"]', 'req.headers["x-jwt-assertion-original"]'],
@@ -28,5 +28,3 @@ export function ByuLogger (options?: LoggerOptions): Logger {
   const opts: LoggerOptions = options == null ? defaultOptions : deepmerge(defaultOptions, options)
   return pino(opts)
 }
-
-export default ByuLogger
--- src/util.ts
+++ src/util.ts
@@ -6,15 +6,15 @@ const ENV_LEVELS: Record<string, Pino.LevelWithSilent> = {
   default: 'debug'
 }
 
-export function getLevel (level: string = 'default'): Pino.LevelWithSilent {
+export function getLevel(level: string = 'default'): Pino.LevelWithSilent {
   return ENV_LEVELS[level] ?? ENV_LEVELS.default
 }
 
-export function isProduction (): boolean {
+export function isProduction(): boolean {
   return process.env.NODE_ENV === 'production'
 }
 
-export function isInstalled (name: string): boolean {
+export function isInstalled(name: string): boolean {
   try {
     return require(name) != null
   } catch (e) {
@@ -25,10 +25,10 @@ export function isInstalled (name: string): boolean {
   }
 }
 
-export function isRecord (value: unknown): value is Record<string, unknown> {
+export function isRecord(value: unknown): value is Record<string, unknown> {
   return typeof value === 'object' && value !== null
 }
 
-export function hasProperty<T = unknown> (value: T, prop: string | number | symbol): prop is keyof T {
+export function hasProperty<T = unknown>(value: T, prop: string | number | symbol): prop is keyof T {
   return Object.hasOwnProperty.call(value, prop)
 }
